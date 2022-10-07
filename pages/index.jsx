b0ef340e336e3d51ff6ec30cc04071513c80ed39
Jack Paget <jackpaget1@gmail.com>
2022-10-07 20:47:15 +0100

    Added ChakraUI

---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Portfolio</title>
+        <meta
+          name='description'
+          content="Jack Paget's web development portfolio"
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+    </div>
+  );
+}
+1;
